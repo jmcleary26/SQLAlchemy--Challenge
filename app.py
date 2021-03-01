4738a96cf9d94390e923c8d21ee21da3fb5d85fc
jmcleary26 <jacquelyncleary@gmail.com>
2021-03-01 14:04:09 -0500

    Updated jupyter notebook

--- app.py
+++ app.py
@@ -72,5 +72,79 @@ def stations():
 
     session.close()
 
+    stationslist = []
+    for station in results:
+        station_dict = {}
+        station_dict["Name"] = name
+        stationslist.append(station_dict)
+
+    return jsonify(stations)
+
+@app.route('/api/v1.0/tobs')
+mostactivestation = 'USC00519281'
+
+def tobs():
+    session = Session(engine)
+
+    latestdatestr = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    latestdatedt = dt.datetime.strptime(latestdatestr[0], '%Y-%m-%d')
+    querydate = dt.date(latestdatedt.year -1, latestdatedt.month, latestdatedt.day)
+
+    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= querydate).filter(Measurement.station == mostactivestation).all()
+
+    session.close()
+
+    tobslist = []
+    for date, tobs in results:
+        tobs_dict = {}
+        tobs_dict["Date"] = date
+        tobs_dict["Tobs"] = tobs
+        tobslist.append(tobs_dict)
+
+    return jsonify(tobslist)
+
+@app.route('/api/v1.0/<start>')
+def startdate(start):
+    session = Session(engine)
+
+    results = session.query(func.min(Measurement.tobs), func.ax(Measurement.tobs), func.avg(Measurement.tobs)).filter(Measurement.date >= start).all()
+
+    session.close()
+
+    tobsstart = []
+    for min, max, avg in results:
+        tobsstartdict = {}
+        tobsstartdict["Min"] = min
+        tobsstartdict["Max"] = max
+        tobsstartdict["Average"] = avg
+        tobsstart.append(tobsstartdict)
+
+    return jsonify(tobsstart)
+
+@app.route('/api/v1.0/<start>/<end>')
+def startenddate(start,stop):
+    session = Session(engine)
+
+    results = session.query(func.min(Measurement.tobs), func.ax(Measurement.tobs), func.avg(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= stop).all()
+
+    session.close()
+
+    tobsstartend = []
+    for min, max, avg in results:
+        tobsstartend_dict = {}
+        tobsstartend_dict["Min"] = min
+        tobsstartend_dict["Max"] = max
+        tobsstartend_dict["Average"] = avg
+        tobsstartend.append(tobsstartend_dict)
+
+    return jsonify(tobsstartend)
+
+
+
+
+
+
     
+
+
 
